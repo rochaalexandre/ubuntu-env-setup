e8b0992b387c5239bda2bd71dd4a466363f7d4be
Alexandre Rocha <alexandre.rocha@clevertech.biz>
2024-07-24 11:51:44 -0300

    feat: add debug logs

--- install/desktop/app-tilix.sh
+++ install/desktop/app-tilix.sh
@@ -2,9 +2,10 @@
 sudo apt install tilix -y
 #Download Tilix theme
 wget https://github.com/dracula/tilix/archive/master.zip
-unzip -o master.zip -d .config/tilix/schemes/
-mv tilix-master/Dracula.json .config/tilix/schemes/
-rm -r .config/tilix/schemes/tilix-master/
+unzip -o master.zip -d "$HOME/.config/tilix/schemes/"
+mkdir -P "$HOME/.config/tilix/schemes/"
+mv tilix-master/Dracula.json "$HOME/.config/tilix/schemes/"
+rm -r "$HOME/.config/tilix/schemes/"
 
 #Removing zip file
 rm master.zip
