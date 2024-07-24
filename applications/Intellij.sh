5845dde28851b00a283ead2e164ae63cb2eb2b39
Alexandre Rocha <alexandre.rocha@clevertech.biz>
2024-07-24 18:06:52 -0300

    feat: update scripts

---
+++ applications/Intellij.sh
@@ -0,0 +1,11 @@
+[Desktop Entry]
+Name=IntelliJ IDEA Ultimate
+Exec="$HOME/.local/intellij/bin/idea.sh" %u
+Version=1.0
+Type=Application
+Categories=Development;IDE;
+Terminal=false
+Icon="$HOME/.local/intellij/bin/idea.svg"
+Comment=The Leading Java and Kotlin IDE
+StartupWMClass=jetbrains-idea
+StartupNotify=true
--- install/desktop/app-intellij.sh
+++ install/desktop/app-intellij.sh
@@ -1,11 +1,20 @@
-# Define IntelliJ IDEA download URL
-IDEA_URL="https://download.jetbrains.com/product?code=IU&latest&distribution=linux"
-mkdir -p ~/.local
-cd ~/.local
-wget -O ideaIU-latest.tar.gz $IDEA_URL
-tar -xzf ideaIU-latest.tar.gz --transform='s/^idea-IU.*/idea-IU/'
-mkdir -p ~/.local/bin
-ln -sf ~/.local/idea-IU/bin/idea.sh ~/.local/bin/idea
+echo "Downloading IntelliJ IDEA..."
+wget -O /tmp/ideaIU-latest.tar.gz "https://download.jetbrains.com/product?code=IU&latest&distribution=linux"
+
+
+TARGET_DIR="$HOME/.local/intellij"
+mkdir -p "$TARGET_DIR"
+
+# Extract the tarball to the target directory
+echo "Extracting IntelliJ IDEA..."
+tar -xzf /tmp/ideaIU-latest.tar.gz -C "$TARGET_DIR" --strip-components=1
+
+# Clean up the tarball
+rm /tmp/ideaIU-latest.tar.gz
+
+
+
+ln -sf "$TARGET_DIR/bin/idea.sh" ~/.local/bin/idea
 rm ideaIU-latest.tar.gz
-echo "IntelliJ IDEA has been installed successfully. You can run it using the command 'idea'."
+echo "IntelliJ IDEA has been installed successfully to $TARGET_DIR. You can run it using the command 'idea'."
 cd -
