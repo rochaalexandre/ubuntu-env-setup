ff860fe80e8d7658f7fabef7c5cbcfa132e018ab
Alexandre Rocha <alexandre.rocha@clevertech.biz>
2024-07-24 11:39:28 -0300

    feat: update scripts

--- install/terminal.sh
+++ install/terminal.sh
@@ -7,6 +7,7 @@ sudo apt install -y curl git unzip
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do
     echo "##### Running installer: $installer"
     source "$installer"
+    wait
 done
 
 echo "All installers have been run."
