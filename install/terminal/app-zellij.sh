68d3a4cd6fee7fb42d2f154cc59817b0ebb9fdfe
Alexandre Rocha <alexandre.rocha@clevertech.biz>
2024-07-24 11:34:04 -0300

    feat: update scripts

--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -4,7 +4,3 @@ tar -xf zellij.tar.gz zellij
 sudo install zellij /usr/local/bin
 rm zellij.tar.gz zellij
 cd -
-
-mkdir -p ~/.config/zellij/themes
-[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp ~/.local/share/omakub/configs/zellij.kdl ~/.config/zellij/config.kdl
-cp ~/.local/share/omakub/themes/tokyo-night/zellij.kdl ~/.config/zellij/themes/tokyo-night.kdl
